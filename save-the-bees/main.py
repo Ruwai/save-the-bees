18b5b065d80decde4bb464d0dbab51bf6c94d9fa
Ruwai <chrisl6uie@gmail.com>
2019-04-19 23:20:31 -0700

    cmdloop

--- save-the-bees/main.py
+++ save-the-bees/main.py
@@ -2,4 +2,30 @@ import click
 import os
 import crayons
 from pathlib import Path
+from bees_shell import SaveTheBees
+
+@click.group()
+@click.version_option()
+def cli():
+    '''
+      Want to know how to save the bees?
+      Open up the shell environment with the command:
+
+      save-the-bees shell
+
+      Enjoy!
+    '''
+    pass
+
+@cli.command('setup')
+    '''
+      :setup:
+      Setup
+    '''
+def setup():
+    pass
+
+@cli.command('shell')
+def shell():
+    SaveTheBees().cmdloop()
 

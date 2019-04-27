4ecec594bbc90935dc8d7636b38161ca959eff62
Ruwai <chrisl6uie@gmail.com>
2019-04-27 13:34:56 -0700

    SimPy

---
+++ save-the-bees/scripts/simulation.py
@@ -0,0 +1,16 @@
+'''
+	This script will be the main script for creating 
+	executable commands for the simulation of CCD
+	
+	The main library I will be using will be SimPy, a 'discrete event simulation'
+	for Python. There will also be an example simulation script in this 
+	scripts directory that will not be routed to the main functionality of this 
+	project.
+
+'''
+
+import pandas as pd 
+import numpy as np 
+import simpy
+import scipy.stats as stats
+

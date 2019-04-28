213e060f64eb02b2ca846779013daa51c8fcf63a
Ruwai <chrisl6uie@gmail.com>
2019-04-27 22:10:56 -0700

    saved funcitons

--- save-the-bees/scripts/functions.py
+++ save-the-bees/scripts/functions.py
@@ -6,3 +6,12 @@ import pandas as pd
 import numpy as np
 import scipy.stats as stats
 
+class F(object):
+
+    def __init__(self, mu):
+        self.mu = mu
+
+    def stnderr(self, a, b, p, q):
+
+        mu = self.a + np.divide(((self.b - self.a)*self.p), (self.p+self.q))
+        return mu

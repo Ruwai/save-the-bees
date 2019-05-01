096427b9afcab87ee5016465bfc07f32e7a96f5e
Ruwai <chrisl6uie@gmail.com>
2019-04-30 19:59:18 -0700

    added

--- save-the-bees/scripts/sheep_wolf_simulation.py
+++ save-the-bees/scripts/sheep_wolf_simulation.py
@@ -1,13 +1,3 @@
-'''
-	This script will be the main script for creating
-	executable commands for the simulation of CCD
-
-	The main library I will be using will be SimPy, a 'discrete event simulation'
-	for Python. There will also be an example simulation script in this
-	scripts directory that will not be routed to the main functionality of this
-	project.
-
-'''
 import numpy as np
 import simpy
 import collections as col
